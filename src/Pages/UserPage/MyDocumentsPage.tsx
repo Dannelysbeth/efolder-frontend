fa2f21b34ca7f05c1611a832dc4004d32a78200b
danifoj315 <danifoj315@student.polsl.pl>
2022-12-02 01:14:13 +0100

    delete documents

--- src/Pages/UserPage/MyDocumentsPage.tsx
+++ src/Pages/UserPage/MyDocumentsPage.tsx
@@ -40,6 +40,15 @@ const AnotherUserPage = ({ user }) => {
     console.log("TEST");
     downloadDocumentById(document);
   };
+  const onDocumentDeleteSubmit = (document: any, e) => {
+    e.preventDefault();
+    deleteDocument(document);
+    const newDocList = documents.filter((doc) => doc.id != document.id);
+    setDocuments(newDocList);
+    getDocuments();
+    setDocuments(documents);
+    console.log("TEST 2");
+  };
 
   const getDocuments = () => {
     return fetch(
@@ -63,57 +72,73 @@ const AnotherUserPage = ({ user }) => {
         setError(true);
       });
   };
+  const deleteDocument = (document: any) => {
+    return fetch(
+      `${process.env.REACT_APP_REMOTE_URL}/api/document/${document.id}`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access")}`,
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setDocuments(responseJson);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(true);
+      });
+  };
   const downloadDocumentById = (doc: any) => {
-    return (
-      fetch(
-        `${process.env.REACT_APP_REMOTE_URL}/api/document/download/${doc.id}`,
-        {
-          method: "GET",
-          // mode: "cors",
-          // responseType: "blob",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("access")}`,
-            "Content-Type": "application/pdf",
-            "Content-Disposition": "attachment;filename=report.pdf",
-          },
-        }
-      )
-        // .then((response) => {
-        //   FileDownload(response.blob, "report.pdf");
-        //   setLoading(false);
-        // })
-        // .catch((error) => {
-        //   setLoading(false);
-        //   setError(true);
-        // });
-        .then((response) => response.blob())
-        .then((responseType) => {
-          // Create blob link to download
-          const url = window.URL.createObjectURL(new Blob([responseType]));
-          const link = document.createElement("a");
-          link.href = url;
-          link.setAttribute("download", doc.name);
+    return fetch(
+      `${process.env.REACT_APP_REMOTE_URL}/api/document/download/${doc.id}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access")}`,
+          "Content-Type": "application/pdf",
+          "Content-Disposition": "attachment;filename=report.pdf",
+        },
+      }
+    )
+      .then((response) => response.blob())
+      .then((responseType) => {
+        // Create blob link to download
+        const url = window.URL.createObjectURL(new Blob([responseType]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", doc.name);
 
-          // Append to html link element page
-          document.body.appendChild(link);
+        // Append to html link element page
+        document.body.appendChild(link);
 
-          // Start download
-          link.click();
+        // Start download
+        link.click();
 
-          // Clean up and remove the link
-          link.parentNode.removeChild(link);
-        })
-    );
+        // Clean up and remove the link
+        link.parentNode.removeChild(link);
+      });
   };
   useEffect(() => {
     getDocuments();
+    setDocuments(documents);
   }, []);
 
   return (
     <MDBContainer>
-      <MDBAccordion borderless alwaysOpen initialActive={0}>
-        <MDBAccordionItem collapseId={1} headerTitle="Moje dokumenty">
-          <MDBAccordion borderless alwaysOpen initialActive={1}>
+      <MDBAccordion borderless alwaysOpen initialActive={1}>
+        <MDBAccordionItem
+          initialActive={1}
+          alwaysOpen
+          collapseId={0}
+          headerTitle="Moje dokumenty"
+        >
+          <MDBAccordion borderless initialActive={0}>
             <MDBAccordionItem collapseId={1} headerTitle="A">
               {documents.length === 0 ? (
                 <h3>Brak dokumentów</h3>
@@ -130,17 +155,27 @@ const AnotherUserPage = ({ user }) => {
                     <MDBDropdown className="btn-group-1">
                       <MDBDropdownToggle />
                       <MDBDropdownMenu>
-                        <MDBDropdownItem link>
-                          <Link
-                            className="nav-link active"
-                            to={{
-                              pathname: `http://localhost:8080/api/document/view/${document.id}`,
-                            }}
-                          >
-                            Otwórz{" "}
-                          </Link>
+                        <MDBDropdownItem
+                          link
+                          // size="bg"
+                          onClick={(e) => onDocumentSubmit(document, e)}
+                        >
+                          {/* <Link
+                        className="nav-link active"e
+                        to={{
+                          pathname: `http://localhost:8080/api/document/view/${document.id}`,
+                        }}
+                      > */}
+                          Pobierz
+                          {/* </Link> */}
                         </MDBDropdownItem>
-                        <MDBDropdownItem link>Another action</MDBDropdownItem>
+                        <MDBDropdownItem
+                          link
+                          // size="bg"
+                          onClick={(e) => onDocumentDeleteSubmit(document, e)}
+                        >
+                          Usuń
+                        </MDBDropdownItem>
                         <MDBDropdownItem link>
                           Something else here
                         </MDBDropdownItem>
