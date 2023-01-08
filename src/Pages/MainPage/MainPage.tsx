1b780d29d9a53823d59f65cdf1276cb903af9939
danifoj315 <danifoj315@student.polsl.pl>
2023-01-08 02:01:26 +0100

    finished touches

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Component, ReactNode, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import "../css/app.css";
 
 const MainPage = () => {
